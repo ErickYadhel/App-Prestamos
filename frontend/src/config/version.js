415f80d4373ac42727f2179e1ecf35c6f0fb88c5
Erick Yadhel <79226686+ErickYadhel@users.noreply.github.com>
2026-08-02 01:16:16 -0400

    Add files via upload

--- frontend/src/config/version.js
+++ frontend/src/config/version.js
@@ -10,10 +10,10 @@
 
 const VERSION_CONFIG = {
   // Versión actual del sistema (Ej: 1.0.0, 1.1.5, 2.0.0)
-  version: '1.0.1',
+  version: '1.0.2',
   
   // Fecha de la versión en formato DD-MM-YYYY
-  fecha: '08/07/2026'
+  fecha: '01/08/2026'
 };
 // ============================================
 // DATOS DE VERSION DEL SISTEMA
@@ -22,6 +22,7 @@ const VERSION_CONFIG = {
 // - fecha: Fecha de lanzamiento o actualización de la versión ( "09/06/2026")
 // - esta fecha se muestra en la información del sistema y en la tarjeta de bienvenida, para que los usuarios sepan cuándo se lanzó la versión actual
 // - MODULO DE PRESTAMOS MEJORAS: Se agregaron mejoras en la gestión de préstamos, incluyendo nuevas métricas y filtros para un mejor control y análisis de los préstamos otorgados.
+// - MODULO DE PAGOS MEJORAS: Se implementaron mejoras en el módulo de pagos, incluyendo la integración con nuevas pasarelas de pago y la optimización del proceso de conciliación de pagos.
 // ============================================
 // Función para obtener la versión formateada
 export const getVersionFormatted = () => `v${VERSION_CONFIG.version}`;
